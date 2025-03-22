3b806ca56ecc1ab18bbbaf92dbdd547df4022769
Philip Oesterlin <philip.oesterlin@gmail.com>
2025-03-22 23:33:04 +0100

    add no-auth implementation

--- src/routes/(authenticated)/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutServerLoad } from './$types';
-import { validateAuth } from '$lib/server/util';
-
-export const load: LayoutServerLoad = async (event) => {
-	const locals = validateAuth(event);
-
-	return { user: locals.user };
-};
